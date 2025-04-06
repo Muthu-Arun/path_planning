88e19c03375293404e24115a49931a7c364783a3
Arun <amuthu165@gmail.com>
2025-04-06 19:39:28 +0530

    working with realsense

---
+++ build_gl.sh
@@ -0,0 +1,2 @@
+g++ source/res-gl.cpp -o test_realsense -std=c++17 -lrealsense2-gl -lrealsense2 -lGL -lglfw -lGLU
+g++ source/pointcloud.cpp -o test_realsense -std=c++17 -lrealsense2-gl -lrealsense2 -lGL -lglfw -lGLU
